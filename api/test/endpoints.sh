ca2b303332a7ff9b509ec860d46e095887e0596c
Sami Jakonen <sami.jakonen@tuni.fi>
2022-11-22 15:14:34 +0200

    Update ci

--- api/test/endpoints.sh
+++ api/test/endpoints.sh
@@ -1,23 +1,17 @@
 # Test API endpoints with cURL
 
 # GET health check
-curl -is http://localhost:8083/ | grep '200'
-curl -is http://localhost:8083/ | grep 'up'
+curl -is dind-service:8083 | grep '200'
+curl -is dind-service:8083 | grep 'up'
 
 # GET messages
-# curl -is http://localhost:8083/messages
 
 # GET state
-# curl -is http://localhost:8083/state
 
 # PUT state
-# curl -is http://localhost:8083/state
 
 # GET run-log
-# curl -is http://localhost:8083/run-log
 
 # GET node-statistic
-# curl -is http://localhost:8083/node-statistic
 
 # GET queue-statistic
-# curl -is http://localhost:8083/queue-statistic
