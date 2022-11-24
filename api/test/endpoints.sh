aae198331d8e8275b82a8b509b9469127781e7f6
Sami Jakonen <sami.jakonen@tuni.fi>
2022-11-24 12:11:29 +0200

    Add tests for /state

--- api/test/endpoints.sh
+++ api/test/endpoints.sh
@@ -9,11 +9,14 @@ curl -is dind-service:8083/messages | grep '200' && echo 'SUCCESS!'
 curl -is dind-service:8083/messages | grep -E -i '^.[0-9]*\-.[0-9]*\-.*Z\ [0-9]*.*MSG.*' && echo 'SUCCESS!'
 
 # GET state
-# curl -is dind-service:8083/state | grep '200' && echo 'SUCCESS!'
-# curl -is dind-service:8083/state | grep 'up' && echo 'SUCCESS!'
+curl -is dind-service:8083/state | grep '200' && echo 'SUCCESS!'
+curl -is dind-service:8083/state | grep 'RUNNING' && echo 'SUCCESS!'
 
 # PUT state
-# curl -is dind-service:8083/state | grep '201' && echo 'SUCCESS!'
+curl -is -X PUT dind-service:8083/state -d PAUSED | grep '201' && echo 'SUCCESS!'
+curl -is dind-service:8083/state | grep 'PAUSED' && echo 'SUCCESS!'
+curl -is -X PUT dind-service:8083/state -d RUNNING | grep '201' && echo 'SUCCESS!'
+curl -is dind-service:8083/state | grep 'RUNNING' && echo 'SUCCESS!'
 
 # GET run-log
 # curl -is dind-service:8083/run-log | grep '200' && echo 'SUCCESS!'
