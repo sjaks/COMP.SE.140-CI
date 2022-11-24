aeae1add57e990a47ef519ecaafbcf6d3544d54d
Sami Jakonen <sami.jakonen@tuni.fi>
2022-11-24 10:33:05 +0200

    Implement /messages

--- api/api.js
+++ api/api.js
@@ -1,8 +1,30 @@
 const http = require('http');
 
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end('up');
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('up');
+    } else if  (req.url === '/messages' && req.method === 'GET') {
+        http.get('http://httpserv:8978/', (resp) => {
+
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(data);
+        });
+
+        }).on('error', (err) => {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Backend error');
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Route not found');
+    }
 };
 
 const server = http.createServer(requestListener);
